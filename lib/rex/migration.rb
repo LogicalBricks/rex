3a35a261c247201fa57efafbc518f66a57b178de
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-10-05 17:34:31 -0500

    Rename attributes when converting them.

--- lib/rex/migration.rb
+++ lib/rex/migration.rb
@@ -2,7 +2,10 @@ module Rex
   class Migration
     def migrate(hash_to_be_converted, config: config)
       hash_to_be_converted.each.with_object({}) do |(k, v), hash|
-        hash.merge! k => v if config.include_original?(k)
+        if config.include_original?(k)
+          key = config.nested_for original: k
+          hash.merge! key.target => v
+        end
       end
     end
   end
--- test/migration_test.rb
+++ test/migration_test.rb
@@ -4,21 +4,28 @@ class MigrationTest < Minitest::Test
   def test_it_generates_a_hash_with_one_attribute
     hash = { name: 'Homer Simpson' }
     expected_hash = { name: 'Homer Simpson' }
-    config = Rex::Attribute.new(:base, nested: [ Rex::Attribute.new(:name)])
+    config = Rex::Attribute.new(:base, nested: [ Rex::Attribute.new(:name) ])
     assert_equal(expected_hash, Rex::Migration.new.migrate(hash, config: config))
   end
 
   def test_it_generates_a_hash_with_two_attributes
     hash = { first_name: 'Homer', last_name: 'Simpson' }
     expected_hash = { first_name: 'Homer', last_name: 'Simpson' }
-    config = Rex::Attribute.new(:config, nested: [ Rex::Attribute.new(:first_name), Rex::Attribute.new(:last_name)])
+    config = Rex::Attribute.new(:config, nested: [ Rex::Attribute.new(:first_name), Rex::Attribute.new(:last_name) ])
     assert_equal(expected_hash, Rex::Migration.new.migrate(hash, config: config))
   end
 
   def test_it_generates_a_hash_without_filtered_attributes
     hash = { first_name: 'Homer', last_name: 'Simpson' }
     expected_hash = { first_name: 'Homer' }
-    config = Rex::Attribute.new(:config, nested: [ Rex::Attribute.new(:first_name)])
+    config = Rex::Attribute.new(:config, nested: [ Rex::Attribute.new(:first_name) ])
+    assert_equal(expected_hash, Rex::Migration.new.migrate(hash, config: config))
+  end
+
+  def test_it_generates_a_hash_with_renamed_attributes
+    hash = { first_name: 'Homer', last_name: 'Simpson' }
+    expected_hash = { firstname: 'Homer', lastname: 'Simpson' }
+    config = Rex::Attribute.new(:config, nested: [ Rex::Attribute.new(:first_name, target: :firstname), Rex::Attribute.new(:last_name, target: :lastname) ])
     assert_equal(expected_hash, Rex::Migration.new.migrate(hash, config: config))
   end
 end
